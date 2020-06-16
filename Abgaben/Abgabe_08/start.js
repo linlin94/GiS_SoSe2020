7519305d8bb09114110f54272170ca6935466229
Linda Koziol <linda.gabriele.koziol@hs-furtwangen.de>
2020-06-16 20:12:48 +0200

    Heroku & Node.js vorbereitet

---
+++ Abgaben/Abgabe_08/start.js
@@ -0,0 +1,6 @@
+"use strict";
+var Abgabe08;
+(function (Abgabe08) {
+    console.log("Es funktioniert!");
+})(Abgabe08 || (Abgabe08 = {}));
+//# sourceMappingURL=start.js.map
---
+++ Abgaben/Abgabe_08/start.ts
@@ -0,0 +1,5 @@
+namespace Abgabe08 {
+
+    console.log("Es funktioniert!");
+    
+}
